df79b8f967e4bde747397ffee6c92cf8f97b9a8c
Karl MacMillan <no@address.here>
2002-08-30 16:30:49 +0000

    Lots of updates. KWM

--- include/knn.hpp
+++ include/knn.hpp
@@ -110,31 +110,75 @@ namespace Gamera {
       double distance;
     };
 
+    namespace {
+
+      class IdStat {
+      public:
+	IdStat() {
+	  min_distance = std::numeric_traits<double>::max();
+	  count = 0;
+	}
+	IdStat(double distance, size_t c) {
+	  min_distance = distance;
+	  count = c;
+	}
+	double min_distance;
+	size_t count;
+      };
+    }
+
     /*
       K NEAREST NEIGHBORS
 
+      This class holds a list of the k nearest neighbors and provides
+      a method of querying for the id of the majority of neighbors. This
+      class is meant to be used once - after calling add for each item in
+      a database and majority the state of the class is undefined. If another
+      search needs to be performed call reset.
     */
     template<class IdType>
     class kNearestNeighbors {
     public:
+      // typedefs for convenience
       typedef IdType id_type;
       typedef Neighbor<IdType> neighbor_type;
       typedef std::vector<neighbor_type> vec_type;
-
-      kNearestNeighbors(size_t k = 1) : m_nn(k) {
+      // Constructor
+      kNearestNeighbors(size_t k = 1) : m_nn(k), m_k(k) {
 	m_additions = 0;
       }
-
+      // Reset the class to its initial state
+      void reset() {
+	m_nn.clear();
+	m_nn.resize(m_k);
+	m_additions = 0;
+      }
+      /*
+	Attempt to add a neighbor to the list of k closest
+	neighbors. The list of neighbors is always kept sorted
+	so that the largest distance is the last element.
+      */
       void add(double distance, const id_type& id) {
 	m_additions++;
-	size_t last = knn.size() - 1;
-	if (distance < m_nn[last].distance) {
-	  m_nn[last].distance = distance;
-	  m_nn[last].id = id;
+	if (distance < m_nn.back().distance) {
+	  m_nn.back().distance = distance;
+	  m_nn.back().id = id;
 	  std::sort(m_nn.begin(), m_nn.end());
 	}
       }
+      /*
+	Find the id of the majority of the k nearest neighbors. This
+	includes tie-breaking if necessary.
+      */
       id_type majority() {
+	/*
+	  make certain that we have at least k valid
+	  entries in the list of neighbors. This is probably not
+	  a problem most of the time, but it is important to
+	  check.
+	*/
+	if (m_additions < m_nn.size())
+	  m_nn.resize(m_additions);
 	// short circuit for k == 1
 	if (m_nn.size() == 1)
 	  return m_nn[0];
@@ -144,10 +188,17 @@ namespace Gamera {
 	  if id_type was an integer there is no garuntee that they are small,
 	  ordered numbers (making a vector impractical).
 	*/
-	typedef std::multimap<id_type, size_t> map_type;
+	typedef std::map<id_type, IdStat> map_type;
 	map_type id_map;
 	for (vec_type::iterator i = m_nn.begin(); i != m_nn.end(); ++i) {
-	  id_map[i->id] += 1;
+	  id_map::iterator current = id_map.find(i->id);
+	  if (current == id_map.end()) {
+	    id_map.insert(std::pair<id_type,
+			  IdStat>(i->id, IdStat(i->distance, 1)));
+	  } else {
+	    current->second.count++;
+	    
+	  }
 	}
 	/*
 	  Now that we have the histogram we can take the majority if there
@@ -156,21 +207,36 @@ namespace Gamera {
 	if (id_map.size() == 1)
 	  return id_map.begin()->first;
 	else {
+	  /*
+	    Find the id(s) with the maximum
+	  */
+	  std::vector<map_type::iterator> max;
+	  max.push_back(id_map.begin());
+	  for (map_type::iterator i = id_map.begin() + 1;
+	       i != id_map.end(); ++i) {
+	    if (i->second > max[0]->second) {
+	      max.clear();
+	      max.push_back(i);
+	    } else if (i->second == max[0]->second) {
+	      max.push_back(i);
+	    }
+	  }
+	  /*
+	    If the vector only has 1 element there are no ties and
+	    we are done.
+	  */
+	  if (max.size() == 1)
+	    return max[0]->first;
+	  else {
+	    
+	  }
+	}
+      }
 
-	}
-	std::vector<map_type::iterator> max;
-	max.push_back(id_map.begin());
-	for (map_type::iterator i = id_map.begin() + 1; i != id_map.end(); ++i) {
-	  if (i->second > max->second)
-	    max = i;
-	}
-      }
-      std::pair<id_type, double> majority_with_distance() {
-	id_type maj = majority();
-      }
     private:
       std::vector<neighbor_type> m_nn;
-      size_t m_additions;
+      size_t m_additions; // counter for the number of neighbors added
+      size_t m_k;
     }
 
   } // namespace kNN

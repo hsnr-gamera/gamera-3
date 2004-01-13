9483cf5cd9dd04131948acda8717853d744ff021
Michael Droettboom <mdboom@gmail.com>
2004-01-13 20:54:52 +0000

    PNG support disabled for Windows

--- gamera/plugins/png_support.py
+++ gamera/plugins/png_support.py
@@ -18,45 +18,49 @@
 #
 
 from gamera.plugin import *
+import sys
+if sys.platform != 'win32':
+    class PNG_info(PluginFunction):
+        """Returns an ``ImageInfo`` object describing a PNG file.
 
-class PNG_info(PluginFunction):
-    """Returns an ``ImageInfo`` object describing a PNG file.
+    *image_file_name*
+      A PNG image filename"""
+        self_type = None
+        args = Args([String("image_file_name")])
+        return_type = ImageInfo("PNG_info")
+    PNG_info_class = PNG_info
+    PNG_info = PNG_info()
 
-*image_file_name*
-  A PNG image filename"""
-    self_type = None
-    args = Args([String("image_file_name")])
-    return_type = ImageInfo("PNG_info")
-PNG_info_class = PNG_info
-PNG_info = PNG_info()
+    class load_PNG(PluginFunction):
+       """Loads a PNG format image file."""
+       self_type = None
+       args = Args([FileOpen("image_file_name", "", "*.png"),
+                    Choice("storage format", ["DENSE", "RLE"])])
+       return_type = ImageType([ONEBIT, GREYSCALE, GREY16, RGB, FLOAT])
+       def __call__(filename, compression = 0):
+          from gamera.plugins import _png_support
+          return _png_support.load_PNG(filename, compression)
+       __call__ = staticmethod(__call__)
+       loads_extensions = ['png']
+    load_PNG_class = load_PNG
+    load_PNG = load_PNG()
 
-class load_PNG(PluginFunction):
-   """Loads a PNG format image file."""
-   self_type = None
-   args = Args([FileOpen("image_file_name", "", "*.png"),
-                Choice("storage format", ["DENSE", "RLE"])])
-   return_type = ImageType([ONEBIT, GREYSCALE, GREY16, RGB, FLOAT])
-   def __call__(filename, compression = 0):
-      from gamera.plugins import _png_support
-      return _png_support.load_PNG(filename, compression)
-   __call__ = staticmethod(__call__)
-   loads_extensions = ['png']
-load_PNG_class = load_PNG
-load_PNG = load_PNG()
+    class save_PNG(PluginFunction):
+       """Saves the image to a PNG format file."""
+       self_type = ImageType(ALL)
+       args = Args([FileSave("image_file_name", "image.png", "*.png")])
 
-class save_PNG(PluginFunction):
-   """Saves the image to a PNG format file."""
-   self_type = ImageType(ALL)
-   args = Args([FileSave("image_file_name", "image.png", "*.png")])
-
-class PngSupportModule(PluginModule):
-    category = "File"
-    cpp_headers = ["png_support.hpp"]
-    extra_libraries = ["png"]
-    functions = [save_PNG, PNG_info_class, load_PNG_class]
-    author = "Michael Droettboom"
-    url = "http://gamera.dkc.jhu.edu/"
-module = PngSupportModule()
-
+    class PngSupportModule(PluginModule):
+        category = "File"
+        cpp_headers = ["png_support.hpp"]
+        extra_libraries = ["png"]
+        functions = [save_PNG, PNG_info_class, load_PNG_class]
+        author = "Michael Droettboom"
+        url = "http://gamera.dkc.jhu.edu/"
+    module = PngSupportModule()
+else:
+    class PngSupportModule(PluginModule):
+        pure_python = True
+    module = PngSupportModule()
                          
          

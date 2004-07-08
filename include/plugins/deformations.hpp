9e8d0aeee677f2d094a0c42a895a99d9e1cc81c9
Albert Brzcecko <no@address.here>
2004-07-08 19:36:59 +0000

    Greatly improved image quality of rotation function (as well as renamed the function from rotateShear to rotate).

--- include/plugins/deformations.hpp
+++ include/plugins/deformations.hpp
@@ -14,13 +14,11 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
-#ifndef kwm12032001_deformations
-#define kwm12032001_deformations
-#define PI 3.1415926535897932384626433832795
+#ifndef _kwm12032001_deformations
+#define _kwn12032001_deformations
+#define FILT_WIDTH 9
+#define PI 3.141592653589793238462643382795
 #include "gamera.hpp"
 #include "gamera_limits.hpp"
 #include "morphology.hpp"
@@ -36,11 +34,11 @@
 #include <iostream>
 #include <fstream>
 using namespace vigra;
-
+using namespace std;
 namespace Gamera
 {
-	template<class T>
-	Image* rotateShear(T &m, float hypot) {
+	template<class T, class U>
+	Image* rotate(T &m, float hypot, U bgcolor) {
 		typedef ImageFactory<T> fact;
 		typename fact::data_type* out_data;
 		typename fact::view_type* out;
@@ -88,16 +86,18 @@ namespace Gamera
 		//Angle is now within [-45..45]
 		//rotate by remaining arbitrary angle
 
-		out = rot45(m, out_data, out, hypot);
+		out = rot45(m, out_data, out, hypot, bgcolor);
 
 		image_copy_attributes(m, *out);
 
 		return out;
 	}
 
-	template<class T, class U, class V>
-	U* rot45(V v, T* m, U* img, float angle)
+	template<class T, class U, class V, class W>
+	U* rot45(V v, T* m, U* img, float angle, W bgcolor)
 	{
+		size_t i;
+		
 		/*
 		 * Performs rotation within -45 and +45 degree angles by applying three shears to the
 		 * image.  The first is a horizontal shear defined by tan(angle/2), the second is a vertical
@@ -114,250 +114,193 @@ namespace Gamera
 		using namespace std;
 		
 		typedef typename ImageFactory<V>::view_type::value_type pixelFormat;
-		pixelFormat background = (pixelFormat)(1.0);
-		size_t width1, height1;
-		T* out_data1;
-		U* out1;
+		pixelFormat background = (pixelFormat)(bgcolor);
+		
 		double dRadAngle = angle * PI / double(180);
 		double dSinE = sin (dRadAngle);
 		double dTan = tan (dRadAngle / 2.0);
-		
+		double dCosE = cos(dRadAngle);
 		//------------------------------------------------------------------------------------
 		// First shear--horizontal
 		//------------------------------------------------------------------------------------
-		width1 = img->ncols() + int(double(img->nrows()) * fabs(dTan));
-		height1 = img->nrows();
-
-		out_data1 = new T(height1,width1);
-		out1 = new U(*out_data1, 0, 0, height1, width1);
-
-		size_t i, *iShears = new size_t[height1];//iShears[height1];
-		double* weights=new double[height1];
-		if (dTan >= 0.0) for(i = 0; i<height1; i++) // Positive angle
-		{
-			double d = (double(i) + 0.5) * dTan;
-			int in = int(floor(d));
-			iShears[height1-i-1] = in;
-			weights[height1-i-1] = d - in;
-		}
-		else for(i = 0; i<height1; i++) // Negative angle
-		{
-			double d = (double(i) + 0.5) * -dTan;
-			int in = int(floor(d));
-			iShears[i]=in;
-			weights[i]=d-in;
-		}
-
-		for(i = 0; i<height1; i++) horizShift(img, out1, i, iShears[i], background, weights[i]);
-		delete img;
-		delete m;
-		img = out1;
+		size_t width1 = img->ncols() + size_t(double(img->nrows()) * fabs(dTan));
+		size_t height1 = img->nrows();
 		
+		T* out_data1 = new T(height1,width1);
+		U* out1 = new U(*out_data1, 0, 0, height1, width1);
+		cout << "first allocation done"<<endl;
+		double d;
+		if(dTan >= 0.0) //Positive Angle
+			d = dTan * (height1 + 0.5);
+		else //Negative Angle
+			d = 0;//-dTan * (height1 - 0.5);
+		
+		for(i = 0; i<height1; i++)
+		{
+			size_t in = size_t(floor(d));
+			double weight = d - in;
+			horizShift(img,out1,i,in,background,weight);
+			d -= dTan;
+		}
+
+		cout << "First shear done" << endl;
 		//------------------------------------------------------------------------------------
 		// Second shear--vertical
 		//------------------------------------------------------------------------------------
-		width1 = img->ncols();
-		height1 = size_t(double(img->ncols() * fabs(dSinE))) + img->nrows();
+		size_t width2 = width1;
+		size_t height2 = size_t( double(img->ncols()) * fabs(dSinE) + (dCosE*img->nrows())) + 1;
 
 		// Allocate image for 2nd shear
-		T* oldod = out_data1;
-		out_data1 = new T(height1,width1);
-		out1 = new U(*out_data1, 0, 0, height1, width1);
+		size_t diff = size_t( fabs(dSinE * dTan * img->nrows()) );
+		T* out_data2 = new T(height2,width2);
+		U* out2 = new U(*out_data2, 0, 0, height2, width2);
 		
-		size_t* iShearsV = new size_t[width1];
-		double* weightsV = new double[width1];
-		if (dSinE >= 0.0) for(i = 0; i < width1; i++) //Positive angle
-		{
-			double d = (double(i)+0.5) * dSinE;
-			size_t in = size_t(floor(d));
-			iShearsV[width1 - i - 1] = in;
-		}
-		else for(i = 0; i < width1; i++) //Negative angle
-		{
-			double d = (double(i)+0.5) * -dSinE;
-			size_t in = size_t(floor(d));
-			iShearsV[i] = in;
-			weightsV[i] = d - in;
-		}
-		
-		for (i = 0; i < width1; i++)
-			vertShift(img, out1, i, iShearsV[width1-i-1], background, weightsV[i]);
+		if (dSinE >= 0.0) //Positive angle
+			d = 0;//dSinE * (img->ncols() - 1.0);
+		else //Negative angle
+			d = -dSinE * (width2);
 
-		delete img;
-		delete oldod;
-		img = out1;
+		cout << "starting second shear" << endl;
+		for (i = 0; i < width2; i++)
+		{
+			size_t in = size_t(floor(d));
+			double weight = d - in;
+			if(in < out2->nrows()) vertShift(out1, out2, i, in, background, weight,diff);
+			d += dSinE;
+		}
+		cout << "second shear done" << endl;
 		
-
 		//------------------------------------------------------------------------------------
 		// Third shear--horizontal
 		//------------------------------------------------------------------------------------
-		width1 = img->ncols() + int(double(img->nrows()) * fabs(dTan));
-		height1 = img->nrows();
-
-		oldod = out_data1;
-		out_data1 = new T(height1,width1);
-		out1 = new U(*out_data1, 0, 0, height1, width1);
+		double abstan = fabs(dTan);
+		diff = size_t(abstan * (height2 - img->nrows() + diff));
+		size_t width3 = //size_t(width2 + height2*fabs(dTan) - diff + 1);
+				size_t( img->ncols()*fabs(dCosE) + img->nrows()*fabs(dSinE) ) + 1;
+		size_t height3 = height2;
 		
-		size_t* iShearsH = new size_t[height1];
-		double* weightsH = new double[height1];
-		if (dTan >= 0.0) for(i = 0; i<height1; i++) // Positive angle
+		T* out_data3 = new T(height3,width3);
+		U* out3 = new U(*out_data3, 0, 0, height3, width3);
+		
+		if (dSinE >= 0.0) //Positive Angle
+			d = dTan * height3;
+		else // Negative angle
+			d = 0;
+		
+		for(i=0; i<height3; i++)
 		{
-			double d = (double(i) + 0.5) * dTan;
-			int in = int(floor(d));
-			iShearsH[height1-i-1] = in;
-			weightsH[height1-i-1] = d - in;
-		}
-		else for(i = 0; i<height1; i++) // Negative angle
-		{
-			double d = (double(i) + 0.5) * -dTan;
-			int in = int(floor(d));
-			iShearsH[i]=in;
-			weightsH[i]=d-in;
+			size_t in = size_t(floor(d));
+			double weight = d - in;
+			if(in<out3->ncols()) horizShift(out2, out3, i, in, background, weight,diff);
+			d -= dTan;
 		}
 		
-		for(i = 0; i<height1; i++) horizShift(img, out1, i, iShearsH[i], background, weightsH[i]);
-
-		delete img;
-		delete oldod;
-		U* out = out1;
-		/* Filtering broke the simplistic border removal algorithm, so this will take some work to remedy
-		U* out = removeExcessBorder(out_data1, out1, background);
-		delete out1;
-		delete out_data1;
-		*/
-		return out;
+		cout << "third shear done" << endl;
+	//	delete img;
+	//	delete out1;
+	//	delete out2;
+		return out3;
 	}
-
-	template<class T, class U, class V>
-	U* removeExcessBorder(T* img_data, U* img, V background)
-	{
-		//------------------------------------------------------------------------------------
-		// Prune background deadspace (cl.exe support not yet complete)
-		//------------------------------------------------------------------------------------
-#ifdef _MSC_VER
-		return img;
-#else
-		size_t i=0;
-		size_t height1 = img->nrows(), width1 = img->ncols();
-		double tolerance = 5;
-
-		for(i = 0; i<height1; i++)
-		{
-			size_t j = 0;
-
-			for(;j<width1; j++)
-			{
-				if (!equalwithinpct(img->get(i,j),background,tolerance)) goto a;
-			}
-		}
-a:		size_t newTop = i;
-		
-		for(i = height1-1; i>=0; i--)
-		{
-			size_t j = 0;
-
-			for(;j<width1; j++)
-			{
-				if (!equalwithinpct(img->get(i,j),background,tolerance)) goto b;
-			}
-		}
-b:		size_t newBott = i;
-		
-		for(i = 0; i<width1; i++)
-		{
-			size_t j = 0;
-
-			for(;j<height1; j++)
-			{
-				if (!equalwithinpct(img->get(i,j),background,tolerance)) goto c;
-			}
-		}
-c:		size_t newLeft = i;
-		
-		for(i = width1-1; i>=0; i--)
-		{
-			size_t j = 0;
-
-			for(;j<height1; j++)
-			{
-				if (!equalwithinpct(img->get(i,j),background,tolerance)) goto d;
-			}
-		}
-d:		size_t newRight = i;
-
-		bool dimsChanged = false;
-		if(width1 != newRight - newLeft)
-		{
-			width1 = newRight - newLeft;
-			dimsChanged = true;
-		}
-		if(height1 != newBott - newTop)
-		{
-			height1 = newBott - newTop;
-			dimsChanged = true;
-		}
-
-		if(!dimsChanged) return img;
-
-		T* out_data1 = new T(height1,width1);
-		U* out1 = new U(*out_data1, 0, 0, height1, width1);
-		
-		for(i = newTop; i<newBott; i++)
-		{
-			size_t j = newLeft;
-			for(; j<newRight; j++)
-			{
-				out1->set(i-newTop,j-newLeft, img->get(i,j));
-			}
-		}
-		return out1;
-#endif
-
-	}
+	
 	template<class T, class U>
-	void horizShift(T* orig, T* newbmp, size_t &row, size_t &amount, U bgcolor, double weight)
+	void horizShift(T* orig, T* newbmp, size_t &row, size_t shiftAmount, U bgcolor, double weight, size_t diff=0)
 	{
-		size_t i;
+		typedef typename ImageFactory<T>::view_type::value_type pixelFormat;
+		size_t i, sourceshift = 0;
 		size_t width1 = newbmp->ncols();
-		
-		for(i = 0; i<amount; i++) newbmp->set(row,i,bgcolor);  //leading background
-
-		U filt[3];
-		filt[2] = bgcolor;
-
-		for(; i<orig->ncols()+amount; i++)
+		pixelFormat p0, p1, oldPixel;
+		p0=p1=oldPixel=bgcolor;
+		i=0;
+		if(shiftAmount >= diff) 
+			shiftAmount -= diff;
+		else
 		{
-			filt[0] = (U)orig->get(row, i-amount);
-			pixelfilt(filt,weight);
-			if((i>=0) && (i<width1))
-			{
-				newbmp->set(row,i,filt[0]);
-			}
+			sourceshift = diff - shiftAmount;
+			shiftAmount = 0;
 		}
+		
+		for(; i<shiftAmount; i++)
+			if(i < width1) newbmp->set(row,i,bgcolor);  //leading background
+		
+		//do first pixel a little differently according to the background color
+		borderfunc(p0,p1,oldPixel,orig->get(row,i-shiftAmount+sourceshift),weight,bgcolor);
+		
+		newbmp->set(row,i, //bgcolor!=pixelFormat(0.0) ? orig->get(row,i-shiftAmount+sourceshift) :
+						     	       p0);
+		i++;
+		
+		for(; i<orig->ncols() + shiftAmount - sourceshift; i++)
+		{
+			filterfunc(p0, p1, oldPixel, orig->get(row,i-shiftAmount+sourceshift), weight);
+			if(i>=0 && i<width1) newbmp->set(row,i,p0);
+		}
+		if(i<width1 && bgcolor==pixelFormat(0.0)) newbmp->set(row,i++,oldPixel);
 		for(; i<width1; i++) newbmp->set(row,i,bgcolor); //trailing background
 	}
-
+	
 	template<class T, class U>
-	void vertShift(T* orig, T* newbmp, size_t &col, size_t &amount, U bgcolor, double weight)
+	void vertShift(T* orig, T* newbmp, size_t &col, size_t amount, U bgcolor, double weight, size_t diff)
 	{
-		size_t i;
-		size_t height1 = newbmp->nrows();
-		for(i = 0; i<amount; i++) newbmp->set(i,col,bgcolor);  //leading background
-
-		U filt[3];
-		filt[2] = bgcolor;
-		for(; i<orig->nrows()+amount; i++)
+		size_t i, sourceshift=0;
+		
+		if(amount >= diff)
+			amount -= diff;
+		else
 		{
-			filt[0] = (U)orig->get(i-amount,col);
-			pixelfilt(filt,weight);
-			if((i>=0) && (i<height1))
-			{
-				newbmp->set(i,col,filt[0]);
-			}
+			sourceshift = diff-amount;
+			amount = 0;
 		}
+		size_t height1 = newbmp->nrows();
+		for(i = 0; i<amount; i++) 
+			if(i< height1) newbmp->set(i,col,bgcolor);  //leading background
+			
+		U p0, p1, oldPixel;
+		oldPixel = p0 = p1 = bgcolor;
+		
+		
+		//do first pixel differently in order to initialize filtering
+		borderfunc(p0,p1,oldPixel,orig->get(i-amount+sourceshift,col),weight,bgcolor);
+		newbmp->set(i,col,p0);
+		i++;
+		
+		for(; i<amount + orig->nrows() - sourceshift; i++)
+		{
+			if(i-amount+sourceshift > 0) filterfunc(p0,p1,oldPixel,orig->get(i - amount + sourceshift,col),weight);
+			if(i>=0 && i<height1) newbmp->set(i,col,p0);
+		}
+		if(i<height1) newbmp->set(i++,col,oldPixel);
+		
 		for(; i<height1; i++) newbmp->set(i,col,bgcolor); //trailing background
 	}
-
+	
+        inline void filterfunc(RGBPixel &p0, RGBPixel &p1, RGBPixel &oldPixel, RGBPixel origPixel, double &weight)
+	{
+		p0 = origPixel;
+		p1 = RGBPixel(  GreyScalePixel(p0.red() * weight),
+				GreyScalePixel(p0.green() * weight),
+				GreyScalePixel(p0.blue() * weight));
+		p0 = RGBPixel(  GreyScalePixel(p0.red() - p1.red() + oldPixel.red()),
+				GreyScalePixel(p0.green() - p1.green() + oldPixel.green()),
+				GreyScalePixel(p0.blue() - p1.blue() + oldPixel.blue()));
+		oldPixel = p1;
+	}
+	
+	template<class T>
+	inline void filterfunc(T &p0, T &p1, T &oldPixel, T origPixel, double & weight)
+	{
+		p0 = origPixel;
+		p1 = (T)(p0 * weight);
+		p0 -= (T)(p1 - oldPixel);
+		oldPixel = p1;
+	}
+	
+	template<class T>
+	inline void borderfunc(T& p0, T& p1, T& oldPixel, T origPixel, double& weight, T bgcolor)
+	{
+		filterfunc(p0,p1,oldPixel,origPixel,weight);
+		if(bgcolor!=T(0.0)) p0 = origPixel;
+	}
+	
 	template<class T>
 	Image* wave(T &m, int amplitude, float freq, int direction, int funcType, int offset) {
 		using namespace std;
@@ -546,9 +489,9 @@ d:		size_t newRight = i;
 		for(i = 0; i<m.nrows(); i++) for(j = 0; j<m.ncols();j++)
 		{
 			out->set(i,
-					j,
-					(typename fact::view_type::value_type)m.get(i, j)
-					);
+				 j,
+				 (typename fact::view_type::value_type)m.get(i, j)
+				 );
 		}
 
 		if(!randset)
@@ -571,105 +514,6 @@ d:		size_t newRight = i;
 
 		return out;
 	}
-
-	template<class T>
-	inline void floatgreyfilt(T pixArr[3], double &weight){
-		pixArr[1] = (T)(weight * pixArr[0]);
-
-		pixArr[0] = (T)(pixArr[0] - (pixArr[1] - pixArr[2]));
-
-		pixArr[2] = pixArr[1];
-	}
-	inline void pixelfilt(FloatPixel pixArr[3], double weight) {
-		floatgreyfilt(pixArr, weight);
-	}
-
-	inline void pixelfilt(GreyScalePixel pixArr[3], double &weight) {
-		floatgreyfilt(pixArr, weight);
-	}
-
-	inline void pixelfilt(Grey16Pixel pixArr[3], double &weight) {
-		floatgreyfilt(pixArr, weight);
-	}
-
-	inline void pixelfilt(RGBPixel pixArr[3], double &weight)
-	{
-		pixArr[1] = RGBPixel(GreyScalePixel(pixArr[0].red() * weight),
-							 GreyScalePixel(pixArr[0].green() * weight),
-							 GreyScalePixel(pixArr[0].blue() * weight));
-/*
-		pixArr[0].red() = GreyScalePixel(pixArr[0].red() - (pixArr[1].red() - pixArr[2].red()));
-		pixArr[0].green() = GreyScalePixel(pixArr[0].green() - (pixArr[1].green() - pixArr[2].green()));
-		pixArr[0].blue() = GreyScalePixel(pixArr[0].blue() - (pixArr[1].blue() - pixArr[2].blue()));
-*/
-		pixArr[0] = RGBPixel(GreyScalePixel(pixArr[0].red() - (pixArr[1].red() - pixArr[2].red())),
-							 GreyScalePixel(pixArr[0].green() - (pixArr[1].green() - pixArr[2].green())),
-							 GreyScalePixel(pixArr[0].blue() - (pixArr[1].blue() - pixArr[2].blue())));
-
-		pixArr[2] = pixArr[1];
-	}
-
-	inline void pixelfilt(OneBitPixel pixArr[3], double &weight) {
-		pixArr[1] = (OneBitPixel)(weight * pixArr[0]);
-
-		pixArr[0] = (OneBitPixel)(pixArr[0] - (pixArr[1] - pixArr[2]));
-
-		pixArr[2] = pixArr[1];
-	}
-
-	template<class T, class U>
-	inline bool ewpGeneric(T a, U b, double pct)
-	{
-		pct/=100;
-
-		return (a>=b*(1-pct) && a<=b*(1+pct));
-	}
-	
-	template<class T>
-	inline bool equalwithinpct(RGBPixel a, T b, double pct)
-	{
-		pct/=100;
-		bool red = (a.red()>=b.red()*(1-pct) && a.red()<=b.red()*(1+pct));
-		bool green = (a.green()>=b.green()*(1-pct) && a.green()<=b.green()*(1+pct));
-		bool blue = (a.blue()>=b.blue()*(1-pct) && a.blue()<=b.blue()*(1+pct));
-
-		return red && green && blue;
-	}
-	template<class T, class U>
-	inline bool equalwithinpct(T a, U b, double pct)
-	{
-		pct/=100;
-
-		return (a>=b*(1-pct) && a<=b*(1+pct));
-	}
-/*
-	inline bool equalwithinpct(FloatPixel &a, FloatPixel &b, double &pct)
-	{
-		return ewpGeneric(a,b,pct);
-	}
-	inline bool equalwithinpct(GreyScalePixel &a, GreyScalePixel &b, double &pct)
-	{
-		return ewpGeneric(a,b,pct);
-	}
-
-	inline bool equalwithinpct(Grey16Pixel &a, GreyScalePixel &b, double &pct)
-	{
-		return ewpGeneric(a,b,pct);
-	}
-	inline bool equalwithinpct(RGBPixel &a, RGBPixel &b, double pct)
-	{
-		pct/=100;
-		bool red = (a.red()>=b.red()*(1-pct) && a.red()<=b.red()*(1+pct));
-		bool green = (a.green()>=b.green()*(1-pct) && a.green()<=b.green()*(1+pct));
-		bool blue = (a.blue()>=b.blue()*(1-pct) && a.blue()<=b.blue()*(1+pct));
-
-		return red && green && blue;
-	}
-
-	inline bool equalwithininput(OneBitPixel &a, OneBitPixel &b, double &pct)
-	{
-		return a == b;
-	}*/
 }
 
 #endif

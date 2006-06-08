c300eb9911c03351431cd152c43ce5727fa90be3
Robert Ferguson <no@address.here>
2006-06-08 21:00:16 +0000

    Fixed and to &&.  RF

--- include/plugins/string_io.hpp
+++ include/plugins/string_io.hpp
@@ -72,13 +72,13 @@ bool fill_image_from_string(T &image, PyObject* data_string) {
 Image* _from_raw_string(Point offset, Dim size, 
 			int pixel_type, int storage_format, 
 			PyObject* data_string) {
-  if (pixel_type == ONEBIT and storage_format == RLE) {
+  if (pixel_type == ONEBIT && storage_format == RLE) {
     typedef TypeIdImageFactory<ONEBIT, RLE> factory;
     typedef factory::image_type image_type;
     image_type* image = factory::create(offset, size);
     if (fill_image_from_string(*image, data_string))
       return image;
-  } else if (pixel_type == ONEBIT and storage_format == DENSE) {
+  } else if (pixel_type == ONEBIT && storage_format == DENSE) {
     typedef TypeIdImageFactory<ONEBIT, DENSE> factory;
     typedef factory::image_type image_type;
     image_type* image = factory::create(offset, size);

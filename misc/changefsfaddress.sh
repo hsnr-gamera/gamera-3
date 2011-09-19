e225fee1b66531456472b07e6179ea1154e68c1b
Christoph Dalitz <christoph.dalitz@hs-niederrhein.de>
2011-09-19 07:20:43 +0000

    FSF address corrected

--- misc/changefsfaddress.sh
+++ misc/changefsfaddress.sh
@@ -3,7 +3,7 @@
 OLDADDRESS="59 Temple Place - Suite 330, Boston, MA  02111-1307, USA"
 NEWADDRESS="51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA"
 
-for f in `grep -rl "$OLDADDRESS" src/* include/* gamera/* *.py LICENSE | grep -v '.svn/'`
+for f in `grep -rl "$OLDADDRESS" src/* include/* gamera/* doc/* *.py LICENSE | grep -v '.svn/'`
 #for f in bla.py
 do
 	echo "converting $f"
